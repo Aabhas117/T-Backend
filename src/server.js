3a2e41b592cd03a339466e05bb9b0d9752fc3775
Aabhas117 <aabhas463@gmail.com>
2026-07-29 19:19:07 +0530

    update

--- src/server.js
+++ src/server.js
@@ -2,35 +2,28 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import express from "express";
 import connectDB from "./db/index.js";
+import dotenv from "dotenv";
 // const dns = require("dns");
 import dns from "dns";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+config.config({
+  path: "./.env",
+});
+
 connectDB()
-
-//(error, req, res, next) four type of but we use only two (req, res)
-.then(()=>{
-  app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running at port : ${process.env.PORT}`);
+  //(error, req, res, next) four type of but we use only two (req, res)
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
   })
-})
-.catch((err)=>{
-  console.log("Mongo sb connection failed !!", err);
-})
+  .catch((err) => {
+    console.log("Mongo sb connection failed !!", err);
+  });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 //if ee approach
 /*
 const app = express()(async () => {
