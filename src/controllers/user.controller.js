5504605cfafe4ad7935ed42fd60a0ebef57fd290
Aabhas117 <aabhas463@gmail.com>
2026-08-06 01:23:04 +0530

    add:  all controller code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import {uploadCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadCloudinary } from "../utils/cloudinary.js";
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
   // validation - not empty
@@ -31,23 +32,35 @@ const registerUser = asyncHandler(async (req, res) => {
   const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-if(!avatarLocalPath){
-  throw new ApiError(400, "Avatar file is required")
-}
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
 
-const avatar = await uploadOnCloudinary(avatarLocalPath)
-const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-if(!avatar){
-  throw new ApiError(400, "Avatar file is required");
-}
-   User.create({
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+  User.create({
     fullName,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
     email,
     password,
-    username: username.toLowerCase()
-   })
+    username: username.toLowerCase(),
+  });
+
+  const createUser = await User.findById(User._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(400, "Something went wrong while registering the user.");
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully.")
+  )
+
   User.findOne({});
 });
 export { registerUser };
