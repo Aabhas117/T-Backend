554d2308cb172aea15dd4252cacea002e3b6a4c5
Aabhas117 <aabhas463@gmail.com>
2026-08-08 23:08:44 +0530

    Add getWatchHistory endpoints

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new Schema({
+const subscriptionSchema = new Schema(
+  {
     subscriber: {
-        type: Schema.types.ObjectId, //one who is subscribing
-        ref: "USer"
+      type: Schema.types.ObjectId, //one who is subscribing
+      ref: "USer",
     },
     channel: {
-        type: Schema.types.ObjectId, // one to whom 'subscriber' is subscribing
-        ref: "User"
-    }
-}, {timestamps: true}) 
+      type: Schema.types.ObjectId, // one to whom 'subscriber' is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-
-
-export const subscription = mongoose.model("Subscription", subscriptionSchema)
+export const subscription = mongoose.model("Subscription", subscriptionSchema);
